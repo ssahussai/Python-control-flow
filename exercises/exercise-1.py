e41738564d68b3255fcdfa95208d859870debd1d
Salma Khan <hussainsalma37@gmail.com>
2019-12-10 14:42:52 -0600

    completed second exercise

--- exercises/exercise-1.py
+++ exercises/exercise-1.py
@@ -10,3 +10,19 @@
 
 # Hints:  Use the in operator to check if a character is in another string
 #         For example, if some_char in 'abc':
+
+alphabet = input('Please enter a letter from the alphabet (a-z or A-Z):').lower()
+if alphabet == "a":
+    print("The letter a is a vowel")
+elif alphabet == "e":
+    print("The letter e is a vowel")
+elif alphabet == "i":
+    print("The letter i is a vowel")
+elif alphabet == "o":
+    print("The letter o is a vowel")
+elif alphabet == "u":
+    print("The letter u is a vowel")
+else:
+    print("You've entered a consonant!")
+
+
--- exercises/exercise-2.py
+++ exercises/exercise-2.py
@@ -7,3 +7,21 @@
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
 
+#Psyedocode: 
+    # write a code where you will prompt the user to enter a phrase
+    # then calculate the length of the phase or word
+    # print the msg back to the user saying "What you entered is xx characters long"
+
+words = None
+while words != "quit":
+    words = input('Please enter a word or phrase:')
+    if words != "quit": # w/o while loop exits but print the msg below 
+        print(f'What you have entered is {len(words)} characters long')
+
+    
+
+
+
+
+
+    
