14a9caabd763955f46da993d70f05b1b9a6b9e3e
Salma Khan <hussainsalma37@gmail.com>
2019-12-11 17:26:19 -0600

    completes exercise 6

--- exercises/exercise-5.py
+++ exercises/exercise-5.py
@@ -14,12 +14,22 @@
 # Hint: The next number is found by adding the two numbers before it
 
 
+terms = int(input("Enter term:"))
+n1 = 0
+n2 = 1
+count = 0
 
+if terms <= 0:
+    print("Enter a positive number!")
+elif terms == 1:
+    print(f"Fibonacci sequence: {n1}")
+elif terms < 51:
+    print("Fibonacci sequences:")
+    while count < terms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2 = nth
+        count += 1
+else: 
+    print("This function goes only upto 50 terms!")
 
-
-
-
-#psedocode:
-# calculate fibonacci sequence
-# print fibonacci sequence
-# print each term and number as follows {term: 0 / number: 0}
